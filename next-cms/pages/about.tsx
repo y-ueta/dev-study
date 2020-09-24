983871d791183338c7ff64d8793d633a0cb11836
y-ueta <ueta@eqs.jp>
2020-09-24 09:36:37 +0900

    'master'

---
+++ next-cms/pages/about.tsx
@@ -0,0 +1,14 @@
+  
+import Head from '../components/templates/head';
+import Navigation from '../components/templates/navigations'
+
+const About = () => {
+  return (
+    <div>
+      <Head title="About page" description="" url="" ogImage="" />
+      <Navigation />
+      <p>About page</p>
+    </div>
+  )
+}
+export default About
